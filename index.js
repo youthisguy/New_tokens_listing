ba2f2877a1e522e02640a0c07a2da83abbed9492
Sam Steel <samuel.ini145@gmail.com>
2024-09-19 15:51:43 +0100

    redundant

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
 const express = require("express");
 const expressApp = express();
-const axios = require("axios");
-const path = require("path");
 const port = process.env.PORT || 3000;
-expressApp.use(express.static("static"));
 expressApp.use(express.json());
 require("dotenv").config();
 const { fetchData } = require("./coingecko");
-
 const { Telegraf } = require("telegraf");
-
 const bot = new Telegraf(process.env.BOT_TOKEN);
-
-expressApp.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "/index.html"));
-});
 
 bot.command("start", (ctx) => {
     console.log(ctx.from);
